1d281ceecc8244782f0731aa6f212a67967f08b0
ayhamhashesh <ayham.hashesh@gmail.com>
2022-02-18 22:13:18 +0200

    Update the responsiveness in the projects and add work experiences

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7d592104d274ff614c71465918b73670",
-"/": "7d592104d274ff614c71465918b73670",
-"main.dart.js": "6be1b81c88b446b4a76e2690f842e088",
+"index.html": "3c67648f6d493426f9627f960dd3eca0",
+"/": "3c67648f6d493426f9627f960dd3eca0",
+"main.dart.js": "f5b8302b7247e75292c52836d98c8c95",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };
