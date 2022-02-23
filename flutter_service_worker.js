2f998fc9cc7190f086c295922c8d5505f1c0cd57
ayhamhashesh <ayham.hashesh@gmail.com>
2022-02-23 18:16:01 +0200

    Correct company name in DevOps Trainee

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "423f00bfa5223b4f82929d9ec0b2658b",
-"/": "423f00bfa5223b4f82929d9ec0b2658b",
-"main.dart.js": "ef11be825a1cc03d8025f1998e5f420e",
+"index.html": "f92dc813e5ed486c8d73032f0692ae45",
+"/": "f92dc813e5ed486c8d73032f0692ae45",
+"main.dart.js": "29a8b68510d88a8ade195f1f61790166",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };
